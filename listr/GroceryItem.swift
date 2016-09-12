447d9c203d44e916970daabab48d65b1c96c5254
ericgiannini <eric.giannini@yahoo.com>
2016-09-12 13:34:14 -0700

    Adding a model

---
+++ listr/GroceryItem.swift
@@ -0,0 +1,13 @@
+//
+//  GroceryItem.swift
+//  listr
+//
+//  Created by Eric Giannini on 9/12/16.
+//  Copyright © 2016 Unicorn Mobile, LLC. All rights reserved.
+//
+
+import UIKit
+
+class GroceryItem: NSObject {
+
+}
