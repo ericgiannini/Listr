5cd3b4040de023566b267f57458eb6f75336fe4d
ericgiannini <eric.giannini@yahoo.com>
2016-09-12 16:02:02 -0700

    Added save and load functions and a explicit initializer.

--- listr/GroceryItem.swift
+++ listr/GroceryItem.swift
@@ -10,13 +10,52 @@ import UIKit
 
 class GroceryItem: NSObject {
     
+    // A property for NSFileManager to supply user's document directories
+    private let fileURL: NSURL = {
+    
+        let documentDirectoryOfURLs = NSFileManager.defaultManager().URLsForDirectory(.DocumentDirectory, inDomains: .UserDomainMask)
+        
+        let documentDirectoryURL = documentDirectoryOfURLs.first!
+        
+        return documentDirectoryURL.URLByAppendingPathComponent("groceryItem.item")
+        
+    }()
+    
     // An empty array of type String for GroceryItems
     private var items: [String] = []
     
+    // An explicit initializer
+    
+    override init() {
+        super.init()
+        loadItems()
+    }
+    
+    // A function for saving grocery items
+    func saveItems() {
+    
+        let itemsArray = items as NSArray
+        
+        print("Saving items to \(fileURL)")
+        
+        if !itemsArray.writeToURL(fileURL, atomically: true) {
+            print("Could not save grocery item")
+        }
+        
+    }
 
+    // A function for loading the array of GroceryItems
+    func loadItems() {
+        if let itemsArray = NSArray(contentsOfURL: fileURL) as? [String] {
+            items = itemsArray
+        }
+    }
+    
+    
     // A function for adding GroceryItems to the list
     func addItem(item: String) {
         items.append(item)
+        saveItems()
     }
     
 }
